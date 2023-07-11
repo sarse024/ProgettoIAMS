8f8a2d64532fc99d6a58d042bd444549e27a11c5
lucafrassinella <119341631+lucafrassinella@users.noreply.github.com>
2023-07-11 17:07:07 +0200

    Add files via upload

--- Funzioni/BiellitticaCircolare.m
+++ Funzioni/BiellitticaCircolare.m
@@ -48,7 +48,7 @@ start = plot3(rI(1), rI(2), rI(3), 'xb', 'LineWidth', 4);
 orbitF = plot3(X,Y,Z, 'LineWidth', 2);
 target = plot3(rF(1), rF(2), rF(3), 'xr', 'LineWidth', 4);
 
-fprintf('IMPLEMENTAZIONE STRATEGIA BIELLITTICA 2:\n\n')
+fprintf('IMPLEMENTAZIONE STRATEGIA BIELLITTICA OTTIMALE:\n\n')
 fprintf(['Parametri orbitali punto di partenza:\n' ...
         '\ta\t\t\te\t\ti\t\tOM\t\tom\t\tth\n ' ...
         '%4.2f\t %.4f\t %2.2f\t %2.2f\t %2.2f\t %2.2f\n\n'], kepI(1), kepI(2), kepI(3),kepI(4),kepI(5), kepI(6))
@@ -71,6 +71,7 @@ e1 = 0;
 [dv1, th1, dt1] = changeOrbitShape(aI, eI, omI, a1, e1, omI, thI, 'apo');
 thman1 = 180;
 dth1 = thman1 - thI;
+t_tot1 = sum(dt1);
 
 % plotting point of circularization
 [r1, v1] = kep2car(aI, eI, iI, OMI, omI, thman1, mu);
@@ -81,7 +82,7 @@ kep1 = [a1, e1, iI, OMI, omI, th1];
 [X,Y,Z] = plotOrbit(kep1,mu,360,precision);
 orbit1 = plot3(X,Y,Z,'--r', 'LineWidth', 1);
 fprintf('\n---- CIRCOLARIZZAZIONE 1 ----\n')
-stampInfoManovra(kep1,thman1, sum(abs(dt1)), sum(abs(dv1)))
+stampInfoManovra(kep1,thman1, sum(abs(dt1)), sum(abs(dv1)), t_tot1)
 
 % Defining parameters for the first half of the bielliptic
 rb = 1.903405340534054e+04; % optimal bielliptic radius computed
@@ -93,20 +94,20 @@ om_t1 = omI + th_in_be;
 
 % Second maneuver: First half of the bielliptic transfer
 [dv2, th2, dt2] = changeOrbitShape(a1, e1, om_t1, a_t1, e_t1, om_t1, th_in_be, 'per');
-
+t_tot2 = sum(dt1) + sum(dt2);
 % Plot and output of the second maneuver:
 kep_t1 = [a_t1, e_t1, iI, OMI, om_t1, th2];
 [X,Y,Z] = plotOrbit(kep_t1,mu,360,precision);
 orbit2 = plot3(X,Y,Z,'--m', 'LineWidth', 1);
 fprintf('\n---- PRIMO IMPULSO BIELLITTICA ----\n')
-stampInfoManovra(kep_t1,th_in_be, sum(abs(dt2)), sum(abs(dv2)))
+stampInfoManovra(kep_t1,th_in_be, sum(abs(dt2)), sum(abs(dv2)), t_tot2)
 % Plot starting point of the bielliptic:
 [r_in_be, v_in_be] = kep2car(a_t1, e_t1, iI, OMI, om_t1, th1, mu);
 in_be = plot3(r_in_be(1), r_in_be(2), r_in_be(3), 'xg', 'LineWidth', 4);
 
 % Third maneuver: change of plane
 [dv3, om3, th3, dt3] = changeOrbitalPlane(a_t1, e_t1, iI, OMI, om_t1, iF, OMF, (th2-1e-7)); 
-
+t_tot3 = sum(dt1) + sum(dt2) + dt3;
 kep2 = [a_t1, e_t1, iF, OMF, om3, th3]; 
 % plotting point of plane change
 [r2, v2] = kep2car(a_t1, e_t1, iF, OMF, om3, th3, mu);
@@ -114,7 +115,7 @@ pt_changePlane = plot3(r2(1), r2(2), r2(3), 'ok', 'LineWidth', 2);
 
 % Output change of plane
 fprintf('\n---- CAMBIO DI PIANO ----\n')
-stampInfoManovra(kep2, th2, dt3,dv3)
+stampInfoManovra(kep2, th2, dt3,dv3, t_tot3)
 
 % Fourth maneuver: second half of the bielliptic
 ra3 = rb;
@@ -123,19 +124,20 @@ a_t2 = (ra3 + rp3)/2;
 e_t2 = (ra3-rp3)/(ra3+rp3);
 
 [dv4, th4, dt4] = changeOrbitShape(a_t1, e_t1, om3, a_t2, e_t2, om3, (th3-1e-7), 'apo');
-
+t_tot4 = sum(dt1) + sum(dt2) + dt3 + sum(dt4);
 % Plotting second half of the bielliptic with new plane and output:
 kep_t2 = [a_t2, e_t2, iF, OMF, om3, th4];
 [X,Y,Z] = plotOrbit(kep_t2,mu,360,precision);
 orbit3 = plot3(X,Y,Z,'--b', 'LineWidth', 1);
 fprintf('\n---- SECONDO IMPULSO BIELLITTICA ----\n')
-stampInfoManovra(kep_t2,th3, sum(abs(dt4)), sum(abs(dv4)))
+stampInfoManovra(kep_t2,th3, sum(abs(dt4)), sum(abs(dv4)), t_tot4)
 
 % Fifth maneuver: ricircularization of the orbit to intersect the final
 % orbit in its apocentre
 a5 = aF*(1+eF); % raggio dell'orbita circolare = raggio dell'apocentro finale
 e5 = 0;
 [dv5, th5, dt5] = changeOrbitShape(a_t2, e_t2, om3, a5, e5, om3, th4, 'per');
+t_tot5 = sum(dt1) + sum(dt2) + dt3 + sum(dt4) + sum(dt5);
 kep5 = [a5, e5, iF, OMF, om3, th5];
 % Plotting point of circularization:
 [r3, v3] = kep2car(a_t2, e_t2, iF, OMF, om3, th4, mu);
@@ -146,10 +148,11 @@ orbit4 = plot3(X,Y,Z,'--k', 'LineWidth', 1);
 
 % Output second circularization
 fprintf('\n---- SECONDA CIRCOLARIZZAZIONE ----\n')
-stampInfoManovra(kep5,th4, sum(dt5), sum(abs(dv5)))
+stampInfoManovra(kep5,th4, sum(dt5), sum(abs(dv5)), t_tot5)
 
 % Sixth maneuver: decircularization into the final orbit
 [dv6, th6, dt6] = changeOrbitShape(a5, e5, omF, aF, eF, omF, th5, 'apo');
+t_tot6 = sum(dt1) + sum(dt2) + dt3 + sum(dt4) + sum(dt5) + sum(dt6);
 kep6 = [aF, eF, iF, OMF, omF, th6];
 
 % Plot point of decircularization
@@ -158,7 +161,7 @@ pt_5 = plot3(r5(1), r5(2), r5(3), 'o', 'LineWidth', 1);
 
 % Output decircularization
 fprintf('\n---- DECIRCOLARIZZAZIONE ----\n')
-stampInfoManovra(kep6,th5, sum(dt6), sum(abs(dv6)))
+stampInfoManovra(kep6,th5, sum(dt6), sum(abs(dv6)), t_tot6)
 
 % Ultimately, we reach the target point
 dt7 = timeOfFlight(aF, eF, th6, thF,mu);
@@ -169,12 +172,11 @@ else
     dTh = thF - th6; 
 end
 
-fprintf('\nAttesa fino al punto finale: %4.2f s\n', dt7)
-
 % Total cost and transfer time:
 dt_tot = sum(dt1) + sum(dt2) + dt3 + sum(dt4) + sum(dt5) + sum(dt6) + dt7;
 dv_tot = sum(abs(dv1)) + sum(abs(dv2)) + abs(dv3) + sum(abs(dv4)) + sum(abs(dv5)) + sum(abs(dv6));
 
+fprintf('\nAttesa fino al punto finale: %4.2f s\n', dt_tot)
 % legend
 legend([orbitI start orbitF target pt_circ1 orbit1 in_be orbit2 pt_changePlane orbit3 pt_circ2 orbit4 pt_5], 'Orbita Iniziale', 'Punto Iniziale', 'Orbita Finale', 'Punto Finale', 'P.to Prima Circolarizzazione', 'Orbita Iniziale Circolarizzata', 'P.to Inizio Biellittica', 'Prima Parte Biellittica', 'P.to Cambio Piano e Cambio Forma', 'Seconda Parte Biellittica', 'P.to Seconda circolarizzazione', 'Seconda Orbita Circolare', 'P.to Decircolarizzazione')
 % Final Output with results:
